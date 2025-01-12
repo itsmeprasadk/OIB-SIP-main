55fb9b567d21b28a3b3af175c4ea1d5a3c329c7e
Prasad Khairnar <prasadofficial247@gmail.com>
2025-01-12 17:44:18 +0530

    Add files via upload

---
+++ Task 3 To do List/todo.js
@@ -0,0 +1,31 @@
+document.querySelector('#push').onclick = function()
+{
+    if(document.querySelector('#newtask input').value.length == 0)
+    {
+        alert("Enter the Task First !!!");
+    }
+    else
+    {   
+        document.getElementById("tasks").style.display = "block";
+        document.querySelector('#tasks').innerHTML += 
+        `
+        <div class="task">
+        <span id="taskname"> 
+        ${document.querySelector('#newtask input').value}
+        </span>    
+        <button class="delete">
+        <i class="far fa-trash-alt"></i>
+        </button>
+        </div>
+        `;
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++)
+        {
+            current_tasks[i].onclick = function()
+            {
+                this.parentNode.remove();
+                document.getElementById("tasks").style.backgroundColor = "none";
+            }
+        }
+    }
+}
